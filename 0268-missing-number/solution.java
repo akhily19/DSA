d107d19fcbccd25cdb5d1bb84c9228be748eff59
akhily19 <akhily2005@gmail.com>
2026-02-21 16:20:38 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 47.4 MB (32.72%)

---
+++ 0268-missing-number/solution.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int n=nums.length;
+        int a=n*(n+1)/2;
+        int b=0;
+        for(int num: nums){
+            b+=num;
+        }
+        return a-b;
+        
+    }
+}
