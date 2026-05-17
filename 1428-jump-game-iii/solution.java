c5400c09bad71bc3f76db7a1f95984c04f0f8610
akhily19 <akhily2005@gmail.com>
2026-05-17 16:46:46 +0000

    Sync LeetCode submission Runtime - 2 ms (80.30%), Memory - 53 MB (16.50%)

---
+++ 1428-jump-game-iii/solution.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean canReach(int[] arr, int start) {
+       boolean[] visited = new boolean[arr.length];
+        return dfs(arr, start, visited);
+    }
+
+    private boolean dfs(int[] arr, int i, boolean[] visited) {
+
+        // Out of bounds
+        if (i < 0 || i >= arr.length) {
+            return false;
+        }
+
+        // Already visited
+        if (visited[i]) {
+            return false;
+        }
+
+        // Reached 0
+        if (arr[i] == 0) {
+            return true;
+        }
+
+        visited[i] = true;
+
+        // Jump forward or backward
+        return dfs(arr, i + arr[i], visited) ||
+               dfs(arr, i - arr[i], visited);  
+    }
+}
